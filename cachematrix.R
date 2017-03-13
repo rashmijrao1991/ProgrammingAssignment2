ef3f8422f39eb03978ea63c4e38e2a910f237d02
Rashmi Rao <rao.307@osu.edu>
2017-03-13 01:54:19 -0400

    R program for the assignment 3

--- cachematrix.R
+++ cachematrix.R
@@ -13,3 +13,53 @@ makeCacheMatrix <- function(x = matrix()) {
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
 }
+
+## makeCacheMatrix: This function creates a special "matrix" object that can cache its inverse.
+# <<- operator is used to assign a value to an object in an 
+#     environment that is different from the current environment
+makeCacheMatrix <- function(x = matrix()) {
+  invmat<-NULL
+  set <- function(y) {
+    x <<- y
+    invmat <<- NULL
+  }
+  get<- function(x) {
+    x
+  }
+  setinverse<-function(y){
+    invmat<<-y
+  }
+  getinverse<-function() {
+    invmat
+  }
+  list(set=set,get=get,setinverse=setinverse,getinverse=getinverse)
+  
+}
+
+
+## cacheSolve: This function computes the inverse of the special "matrix" returned
+# by makeCacheMatrix above. If the inverse has already been calculated 
+# (and the matrix has not changed), then the cachesolve should retrieve the inverse from the cache.
+
+
+cacheSolve <- function(x, ...) {
+  ## Return a matrix that is the inverse of 'x'
+  # x is an object of makeCacheMatrix
+  invmat<-x$getinverse()
+  if(!is.null(invmat)){
+    message("Cached inverse matrix")
+    return(invmat)
+  }
+  data<- x$get()
+  invmat <- solve(data, ...)
+  x$setinverse(invmat)
+  invmat
+  
+}
+
+#to check
+#x1 <- diag(5,3)
+#CachedMarix <- makeCacheMatrix(x1)
+#cacheSolve(CachedMarix)
+
+
